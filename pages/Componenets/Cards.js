5c25c0ac7c8c40687ddc29cf6551114451cb7661
Naitik <naitikraj12935@gmail.com>
2024-01-05 22:00:23 +0530

    change this

--- pages/Componenets/Cards.js
+++ pages/Componenets/Cards.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Cards({ obj }) {
   return (
     <div className={Cardstyle.Card_component}>
-      <Image src={obj?.Url} alt={obj.title} width={25} height={25} />
+      <Image src={obj?.Url} alt={obj?.title} width={25} height={25} />
       <h2 className={Cardstyle.Card_heading}>{obj?.title}</h2>
       <div className={Cardstyle.Card_description}>{obj?.description}</div>
     </div>
